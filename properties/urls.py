09ed5a3bf4a8e7ae65c9ffc389ee6430bccd9432
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-23 17:46:46 +0000

    Template

--- properties/urls.py
+++ properties/urls.py
@@ -1,7 +1,29 @@
 from django.urls import path
-from . import views
+from .views import (
+    PropertyListView, PropertyCreateView, PropertyUpdateView, PropertyDeleteView,
+    TenantListView, TenantCreateView, TenantUpdateView, TenantDeleteView,
+    ContractListView, ContractCreateView, ContractUpdateView, ContractDeleteView,
+    PaymentListView, PaymentCreateView, PaymentUpdateView, PaymentDeleteView
+)
 
 urlpatterns = [
-    path('property/', views.property_list, name='property_list'),
-    path('tenant/<int:pk>/', views.tenant_detail, name='tenant_detail'),
+    path('properties/', PropertyListView.as_view(), name='property_list'),
+    path('properties/add/', PropertyCreateView.as_view(), name='property_add'),
+    path('properties/<int:pk>/edit/', PropertyUpdateView.as_view(), name='property_edit'),
+    path('properties/<int:pk>/delete/', PropertyDeleteView.as_view(), name='property_delete'),
+
+    path('tenants/', TenantListView.as_view(), name='tenant_list'),
+    path('tenants/add/', TenantCreateView.as_view(), name='tenant_add'),
+    path('tenants/<int:pk>/edit/', TenantUpdateView.as_view(), name='tenant_edit'),
+    path('tenants/<int:pk>/delete/', TenantDeleteView.as_view(), name='tenant_delete'),
+
+    path('contracts/', ContractListView.as_view(), name='contract_list'),
+    path('contracts/add/', ContractCreateView.as_view(), name='contract_add'),
+    path('contracts/<int:pk>/edit/', ContractUpdateView.as_view(), name='contract_edit'),
+    path('contracts/<int:pk>/delete/', ContractDeleteView.as_view(), name='contract_delete'),
+
+    path('payments/', PaymentListView.as_view(), name='payment_list'),
+    path('payments/add/', PaymentCreateView.as_view(), name='payment_add'),
+    path('payments/<int:pk>/edit/', PaymentUpdateView.as_view(), name='payment_edit'),
+    path('payments/<int:pk>/delete/', PaymentDeleteView.as_view(), name='payment_delete'),
 ]
--- properties/views.py
+++ properties/views.py
@@ -23,3 +23,136 @@ class PropertyListView(ListView):
         context = super().get_context_data(**kwargs)
         context['form'] = PropertySearchForm(self.request.GET or None)
         return context
+
+class PropertyCreateView(CreateView):
+    model = Property
+    form_class = PropertyForm
+    template_name = 'property_form.html'
+    success_url = reverse_lazy('property_list')
+
+    def form_valid(self, form):
+        messages.sucess(self.request, 'تم إضافة العقار بنجاح')
+        return super().form_valid(form)
+
+class PropertyUpdateView(UpdateView):
+    model = Property
+    form_class = PropertyForm
+    template_name = 'property_form.html'
+    success_url = reverse_lazy('property_list')
+
+    def form_valid(self, form):
+        messages.sucess(self.request, 'تم تحديث العقار بنجاح')
+        return super().form_valid(form)
+
+class PropertyDeleteView(DeleteView):
+    model = Property
+    template_name = 'property_confirm_delete.html'
+    success_url = reverse_lazy('property_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.sucess(self.request, 'تم حذف العقار بنجاح')
+        return super().delete(request, *args, **kwargs)
+
+class TenantListView(ListView):
+    model = Tenant
+    template_name = 'tenant_list.html'
+    context_object_name = 'tenants'
+    paginate_by = 10
+
+class TenantCreateView(CreateView):
+    model = Tenant
+    form_class = TenantForm
+    template_name = 'tenant_form.html'
+    success_url = reverse_lazy('tenant_list')
+
+    def form_valid(self, form):
+        messages.sucess(self.request, 'تم إضافة المستأجر بنجاح')
+        return super().form_valid(form)
+
+class TenantUpdateView(UpdateView):
+    model = Tenant
+    form_class = TenantForm
+    template_name = 'tenant_form.html'
+    success_url = reverse_lazy('tenant_list')
+
+    def form_valid(self, form):
+        messages.sucess(self.request, 'تم تحديث المستأجر بنجاح')
+        return super().form_valid(form)
+
+class TenantDeleteView(DeleteView):
+    model = Tenant
+    template_name = 'tenant_confirm_delete.html'
+    success_url = reverse_lazy('tenant_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.sucess(self.request, 'تم حذف المستأجر بنجاح')
+        return super().delete(request, *args, **kwargs)
+
+class ContractListView(ListView):
+    model = Contract
+    template_name = 'contract_list.html'
+    context_object_name = 'contracts'
+    paginate_by = 10
+
+class ContractCreateView(CreateView):
+    model = Contract
+    form_class = ContractForm
+    template_name = 'contract_form.html'
+    success_url = reverse_lazy('contract_list')
+
+    def form_valid(self, form):
+        messages.sucess(self.request, 'تم إضافة العقد بنجاح')
+        return super().form_valid(form)
+
+class ContractUpdateView(UpdateView):
+    model = Contract
+    form_class = ContractForm
+    template_name = 'contract_form.html'
+    success_url = reverse_lazy('contract_list')
+
+    def form_valid(self, form):
+        messages.sucess(self.request, 'تم تحديث العقد بنجاح')
+        return super().form_valid(form)
+
+class ContractDeleteView(DeleteView):
+    model = Contract
+    template_name = 'contract_confirm_delete.html'
+    success_url = reverse_lazy('contract_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.sucess(self.request, 'تم حذف العقد بنجاح')
+        return super().delete(request, *args, **kwargs)
+
+class PaymentListView(ListView):
+    model = Payment
+    template_name = 'payment_list.html'
+    context_object_name = 'payments'
+    paginate_by = 10
+
+class PaymentCreateView(CreateView):
+    model = Payment
+    form_class = PaymentForm
+    template_name = 'payment_form.html'
+    success_url = reverse_lazy('payment_list')
+
+    def form_valid(self, form):
+        messages.sucess(self.request, 'تم إضافة الدفعة بنجاح')
+        return super().form_valid(form)
+
+class PaymentUpdateView(UpdateView):
+    model = Payment
+    form_class = PaymentForm
+    template_name = 'payment_form.html'
+    success_url = reverse_lazy('payment_list')
+
+    def form_valid(self, form):
+        messages.sucess(self.request, 'تم تحديث الدفعة بنجاح')
+        return super().form_valid(form)
+
+class PaymentDeleteView(DeleteView):
+    model = Payment
+    template_name = 'payment_confirm_delete.html'
+    success_url = reverse_lazy('payment_list')
+    def delete(self, request, *args, **kwargs):
+        messages.sucess(self.request, 'تم حذف الدفعة بنجاح')
+        return super().delete(request, *args, **kwargs)
