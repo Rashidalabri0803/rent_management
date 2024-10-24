be23a2051cbdb158a2be77aa2a51860d6a211ff4
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-24 18:37:45 +0000

    تحسين وإضافة المزيد view.py

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from .models import Property, Tenant, Contract, Payment
-from .forms import PropertyForm, TenantForm, ContractForm, PaymentForm, PropertySearchForm
 from django.contrib import messages
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from .models import Property, Tenant, Contract, Payment, MaintenanceEvent
+from .forms import PropertyForm, TenantForm, ContractForm, PaymentForm, MaintenanceEventForm, PropertySearchForm, TenantSearchForm
 # Create your views here.
 
 class PropertyListView(ListView):
@@ -14,14 +14,20 @@ class PropertyListView(ListView):
     
     def get_queryset(self):
         queryset = Property.objects.all()
-        query = self.request.GET.get('query')
-        if query:
-            queryset = queryset.filter(name__icontains=query)
+        name = self.request.GET.get('name', '')
+        property_type = self.request.GET.get('property_type', '')
+        available = self.request.GET.get('available', '')
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if property_type:
+            queryset = queryset.filter(property_type=property_type)
+        if available:
+            queryset = queryset.filter(available=True)
         return queryset
         
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = PropertySearchForm(self.request.GET or None)
+        context['form'] = PropertySearchForm(self.request.GET)
         return context
 
 class PropertyCreateView(CreateView):
@@ -31,7 +37,7 @@ class PropertyCreateView(CreateView):
     success_url = reverse_lazy('property_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'تم إضافة العقار بنجاح')
+        messages.success(self.request, "تم إضافة العقار بنجاح!")
         return super().form_valid(form)
 
 class PropertyUpdateView(UpdateView):
@@ -41,7 +47,7 @@ class PropertyUpdateView(UpdateView):
     success_url = reverse_lazy('property_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'تم تحديث العقار بنجاح')
+        messages.success(self.request, "تم تحديث العقار بنجاح")
         return super().form_valid(form)
 
 class PropertyDeleteView(DeleteView):
@@ -50,14 +56,38 @@ class PropertyDeleteView(DeleteView):
     success_url = reverse_lazy('property_list')
 
     def delete(self, request, *args, **kwargs):
-        messages.success(self.request, 'تم حذف العقار بنجاح')
+        messages.success(self.request, "تم حذف العقار بنجاح")
         return super().delete(request, *args, **kwargs)
+
+class PropertyDetailView(DetailView):
+    model = Property
+    template_name = 'property_detail.html'
+    context_object_name = 'property'
 
 class TenantListView(ListView):
     model = Tenant
     template_name = 'tenant_list.html'
     context_object_name = 'tenants'
     paginate_by = 10
+
+    def get_queryset(self):
+        queryset = Tenant.objects.all()
+        first_name = self.request.GET.get('first_name', '')
+        last_name = self.request.GET.get('last_name', '')
+        phone = self.request.GET.get('phone', '')
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+        if phone:
+            queryset = queryset.filter(phone__icontains=phone)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TenantSearchForm(self.request.GET)
+        return context
 
 class TenantCreateView(CreateView):
     model = Tenant
@@ -66,7 +96,7 @@ class TenantCreateView(CreateView):
     success_url = reverse_lazy('tenant_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'تم إضافة المستأجر بنجاح')
+        messages.success(self.request, "تم إضافة المستأجر بنجاح")
         return super().form_valid(form)
 
 class TenantUpdateView(UpdateView):
@@ -76,7 +106,7 @@ class TenantUpdateView(UpdateView):
     success_url = reverse_lazy('tenant_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'تم تحديث المستأجر بنجاح')
+        messages.success(self.request, "تم تحديث المستأجر بنجاح")
         return super().form_valid(form)
 
 class TenantDeleteView(DeleteView):
@@ -85,8 +115,13 @@ class TenantDeleteView(DeleteView):
     success_url = reverse_lazy('tenant_list')
 
     def delete(self, request, *args, **kwargs):
-        messages.success(self.request, 'تم حذف المستأجر بنجاح')
+        messages.success(self.request, "تم حذف المستأجر بنجاح")
         return super().delete(request, *args, **kwargs)
+
+class TenantDetailView(DetailView):
+    model = Tenant
+    template_name = 'tenant_detail.html'
+    context_object_name = 'tenant'
 
 class ContractListView(ListView):
     model = Contract
@@ -101,7 +136,7 @@ class ContractCreateView(CreateView):
     success_url = reverse_lazy('contract_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'تم إضافة العقد بنجاح')
+        messages.success(self.request, "تم إضافة العقد بنجاح")
         return super().form_valid(form)
 
 class ContractUpdateView(UpdateView):
@@ -111,7 +146,7 @@ class ContractUpdateView(UpdateView):
     success_url = reverse_lazy('contract_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'تم تحديث العقد بنجاح')
+        messages.success(self.request, "تم تحديث العقد بنجاح")
         return super().form_valid(form)
 
 class ContractDeleteView(DeleteView):
@@ -120,7 +155,7 @@ class ContractDeleteView(DeleteView):
     success_url = reverse_lazy('contract_list')
 
     def delete(self, request, *args, **kwargs):
-        messages.success(self.request, 'تم حذف العقد بنجاح')
+        messages.success(self.request, "تم حذف العقد بنجاح")
         return super().delete(request, *args, **kwargs)
 
 class PaymentListView(ListView):
@@ -136,7 +171,7 @@ class PaymentCreateView(CreateView):
     success_url = reverse_lazy('payment_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'تم إضافة الدفعة بنجاح')
+        messages.success(self.request, "تم إضافة الدفعة بنجاح")
         return super().form_valid(form)
 
 class PaymentUpdateView(UpdateView):
@@ -146,7 +181,7 @@ class PaymentUpdateView(UpdateView):
     success_url = reverse_lazy('payment_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'تم تحديث الدفعة بنجاح')
+        messages.success(self.request, "تم تحديث الدفعة بنجاح")
         return super().form_valid(form)
 
 class PaymentDeleteView(DeleteView):
@@ -154,5 +189,40 @@ class PaymentDeleteView(DeleteView):
     template_name = 'payment_confirm_delete.html'
     success_url = reverse_lazy('payment_list')
     def delete(self, request, *args, **kwargs):
-        messages.success(self.request, 'تم حذف الدفعة بنجاح')
+        messages.success(self.request, "تم حذف الدفعة بنجاح")
+        return super().delete(request, *args, **kwargs)
+
+class MaintenanceEventListView(ListView):
+    model = MaintenanceEvent
+    template_name = 'maintenance_event_list.html'
+    context_object_name = 'maintenance_events'
+    paginate_by = 10
+
+class MaintenanceEventCreateView(CreateView):
+    model = MaintenanceEvent
+    form_class = MaintenanceEventForm
+    template_name = 'maintenance_event_form.html'
+    success_url = reverse_lazy('maintenance_event_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "تم إضافة حدث الصيانة بنجاح")
+        return super().form_valid(form)
+
+class MaintenanceEventUpdateView(UpdateView):
+    model = MaintenanceEvent
+    form_class = MaintenanceEventForm
+    template_name = 'maintenance_event_form.html'
+    success_url = reverse_lazy('maintenance_event_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "تم تحديث حدث الصيانة بنجاح")
+        return super().form_valid(form)
+
+class MaintenanceEventDeleteView(DeleteView):
+    model = MaintenanceEvent
+    template_name = 'maintenance_event_confirm_delete.html'
+    success_url = reverse_lazy('maintenance_event_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "تم حذف حدث الصيانة بنجاح")
         return super().delete(request, *args, **kwargs)
